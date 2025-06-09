53bc36f83b4889d9d5f807291d0167a120f5c89c
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-06-10 01:41:48 +0530

    Finalize backend updates

---
+++ task_manager_wrapper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'lib/Screens/TaskManager.dart';
+
+// TaskManager wrapper class to ensure proper initialization
+class TaskManagerWrapper extends StatelessWidget {
+  const TaskManagerWrapper({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: TaskManager(),
+      ),
+    );
+  }
+}
