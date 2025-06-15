0d3a9815933767da4334f1becf95ac25ba7fcd6f
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-06-15 12:31:44 +0530

    Finalize backend updates

---
+++ test_firebase_service_import.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercomponenets/Services/firebase_service.dart';
+
+// Test file to verify FirebaseService import works
+void main() {
+  print('Testing FirebaseService import...');
+  
+  // Test static method access
+  final userId = FirebaseService.getCurrentUserId();
+  print('Current user ID: $userId');
+  
+  print('FirebaseService import test successful!');
+}
