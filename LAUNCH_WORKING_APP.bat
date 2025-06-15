785a90873738659a15ec7cf5dc5974d5e3b7778f
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-06-15 21:59:16 +0530

    Finalize backend updates

--- LAUNCH_WORKING_APP.bat
+++ LAUNCH_WORKING_APP.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "c:\Users\senes\OneDrive\Desktop\MAD\Team-Sync-Project-Management-Application"
 echo Starting Team Sync on Edge...
-flutter run -d edge
+set CHROME_EXECUTABLE="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+flutter run -d chrome --web-port=8081
