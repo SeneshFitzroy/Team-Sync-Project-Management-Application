bfd9f49daf21e37c78ecc7b48c9122465667c176
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-08-16 02:53:39 +0530

    feat: Enhanced lib/Screens/MainAppNavigator.dart,  - Detailed changes: diff --git a/lib/Screens/MainAppNavigator.dart b/l... at  2:53:39.20 Sat 08/16/2025

---
+++ lib/Screens/MainAppNavigator.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'Dashboard.dart';
+import 'TaskManager.dart';
+import 'Chat.dart';
+import 'Calendar.dart';
+import '../Components/nav_bar.dart';
+
+class MainAppNavigator extends StatefulWidget {
+  const MainAppNavigator({super.key});
+
+  @override
+  State<MainAppNavigator> createState() => _MainAppNavigatorState();
+}
+
+class _MainAppNavigatorState extends State<MainAppNavigator> {
+  int _selectedIndex = 0;
+  
+  final List<Widget> _pages = [
+    const Dashboard(),
+    const TaskManager(),
+    const ChatScreen(),
+    const Calendar(),
+  ];
+
+  void _onNavigationTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavBar(
+        selectedIndex: _selectedIndex,
+        onTap: _onNavigationTap,
+      ),
+    );
+  }
+}
