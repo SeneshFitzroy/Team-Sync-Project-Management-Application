1079b492ae41926e47b517e734fb10936a251e3f
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-08-16 13:04:16 +0530

    feat: Enhanced lib/Screens/login_firebase.dart,  - Detailed changes: diff --git a/lib/Screens/login_firebase.dart b/lib... at 13:04:16.83 Sat 08/16/2025

--- lib/Screens/login_firebase.dart
+++ lib/Screens/login_firebase.dart
@@ -124,9 +124,7 @@ class _LoginPageState extends State<LoginPage> {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => ForgetPasswordPage(
-          initialEmail: _emailController.text.trim(),
-        ),
+        builder: (context) => ForgetPassword(),
       ),
     );
   }
