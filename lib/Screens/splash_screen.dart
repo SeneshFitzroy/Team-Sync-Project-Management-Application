577fa61a3c38b4b92b7d0c113a9c925853049b54
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-08-18 22:57:09 +0530

    feat: Enhanced lib/main_test.dart, lib/Screens/splash_screen.dart,  - Detailed changes: diff --git a/lib/Screens/splash_screen.dart b/lib/... at 22:57:09.77 Mon 08/18/2025

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 import '../widgets/TickLogo.dart';
-import '../widgets/auth_wrapper.dart';
+import 'welcome-page1.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -118,7 +118,7 @@ class _SplashScreenState extends State<SplashScreen>
       Navigator.pushReplacement(
         context,
         PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => const AuthWrapper(),
+          pageBuilder: (context, animation, secondaryAnimation) => const WelcomePage1(),
           transitionDuration: const Duration(milliseconds: 500),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             return FadeTransition(
