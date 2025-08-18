7f3e8e02c8688b446e6131f97d37a85cba460a56
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-08-18 23:46:34 +0530

    feat: Enhanced dataconnect-generated/dart/default_connector/default.dart,  - Detailed changes: diff --git a/dataconnect-generated/dart/default_co... at 23:46:34.93 Mon 08/18/2025

--- dataconnect-generated/dart/default_connector/default.dart
+++ dataconnect-generated/dart/default_connector/default.dart
@@ -12,23 +12,23 @@
 
 
 
-class DefaultConnector {
-  
-
-  static ConnectorConfig connectorConfig = ConnectorConfig(
-    'us-central1',
-    'default',
-    'team-sync-project-management-application',
-  );
-
-  DefaultConnector({required this.dataConnect});
-  static DefaultConnector get instance {
-    return DefaultConnector(
-        dataConnect: FirebaseDataConnect.instanceFor(
-            connectorConfig: connectorConfig,
-            sdkType: CallerSDKType.generated));
-  }
-
-  FirebaseDataConnect dataConnect;
-}
+// class DefaultConnector {
+//   
+// 
+//   static ConnectorConfig connectorConfig = ConnectorConfig(
+//     'us-central1',
+//     'default',
+//     'team-sync-project-management-application',
+//   );
+// 
+//   DefaultConnector({required this.dataConnect});
+//   static DefaultConnector get instance {
+//     return DefaultConnector(
+//         dataConnect: FirebaseDataConnect.instanceFor(
+//             connectorConfig: connectorConfig,
+//             sdkType: CallerSDKType.generated));
+//   }
+// 
+//   FirebaseDataConnect dataConnect;
+// }
 
