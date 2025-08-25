883aa797db30dafd8ac85986b0510d5ece3d4708
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-08-25 08:58:00 +0530

    feat: Enhanced lib/Screens/MainAppNavigator.dart,  - Detailed changes: diff --git a/lib/Screens/MainAppNavigator.dart b/l... at  8:57:59.94 Mon 08/25/2025

--- lib/Screens/MainAppNavigator.dart
+++ lib/Screens/MainAppNavigator.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'Dashboard.dart';
 import 'TaskPage.dart';
-import 'TeamChat.dart';
+import 'TeamChat.dart' as team_chat;
 import 'Calendar.dart';
 import '../Components/nav_bar.dart';
 import 'login-page.dart';
